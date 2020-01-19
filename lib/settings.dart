19d256c81cde720b136e55248fb81c705b700a94
Thomas Borlée <thomas@borlee.be>
2020-01-19 14:39:13 +0100

    Allow user to set home as the current position

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -8,43 +10,99 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  TextEditingController _controller;
+  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  String _home;
   bool value = true;
 
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _retrieveHome();
   }
 
   void dispose() {
-    _controller.dispose();
     super.dispose();
+  }
+
+  Future<void> _retrieveHome() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _home = prefs.get("home");
+    });
+  }
+
+  Future<void> _setHome(Position position) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("home", "${position.latitude},${position.longitude}");
+    setState(() {
+      _home = prefs.get("home");
+    });
+  }
+
+  Future<void> _removeHome() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove("home");
+    setState(() {
+      _home = prefs.get("home");
+    });
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Paramètres")),
-      body:       SettingsList(
-        sections: [
-          SettingsSection(
-            title: 'Section',
-            tiles: [
-              SettingsTile(
-                title: 'Language',
-                subtitle: 'English',
-                leading: Icon(Icons.language),
-                onTap: () {},
-              ),
-              SettingsTile.switchTile(
-                title: 'Use fingerprint',
-                leading: Icon(Icons.fingerprint),
-                switchValue: value,
-                onToggle: (bool value) {},
-              ),
-            ],
-          ),
-        ],
-      )
-    );
+        appBar: AppBar(title: Text("Paramètres")),
+        body: SettingsList(
+          sections: [
+            SettingsSection(
+              title: 'Navigation',
+              tiles: [
+                SettingsTile(
+                  title: 'Domicile',
+                  subtitle: _home != null
+                      ? _home
+                      : "Appuyez ici pour définir la position actuelle comme votre domicile.",
+                  leading: Icon(Icons.home),
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Domicile"),
+                            content: Text(
+                                "Définir la position actuelle comme votre domicile?"),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text("REINITIALISER"),
+                                onPressed: () {
+                                  _removeHome();
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              FlatButton(
+                                child: Text("NON"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              FlatButton(
+                                child: Text("OUI"),
+                                onPressed: () {
+                                  geolocator
+                                      .getCurrentPosition(
+                                          desiredAccuracy:
+                                              LocationAccuracy.high)
+                                      .then((Position position) {
+                                    _setHome(position);
+                                    Navigator.of(context).pop();
+                                  });
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
