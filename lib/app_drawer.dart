9641e3301c2477a62d0b9c65e895068620a312b3
Thomas Borlée <thomas@borlee.be>
2020-02-09 20:34:10 +0100

    Tweak version label position

--- lib/app_drawer.dart
+++ lib/app_drawer.dart
@@ -8,53 +8,59 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-              margin: EdgeInsets.zero,
-              padding: EdgeInsets.zero,
-              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-              child: Stack(children: <Widget>[
-                Positioned(
-                    bottom: 12.0,
-                    left: 16.0,
-                    child: Text("Points Verts Adeps",
-                        style: Theme.of(context).primaryTextTheme.title)),
-              ])),
-          ListTile(
-            leading: Icon(Icons.directions_walk),
-            title: Text('Prochains Points Verts'),
-            onTap: () {
-              Navigator.pushReplacement(
-                  context, MaterialPageRoute(builder: (context) => WalkList()));
-            },
+        child: Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                  margin: EdgeInsets.zero,
+                  padding: EdgeInsets.zero,
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColor),
+                  child: Stack(children: <Widget>[
+                    Positioned(
+                        bottom: 12.0,
+                        left: 16.0,
+                        child: Text("Points Verts Adeps",
+                            style: Theme.of(context).primaryTextTheme.title)),
+                  ])),
+              ListTile(
+                leading: Icon(Icons.directions_walk),
+                title: Text('Prochains Points Verts'),
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => WalkList()));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Paramètres'),
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => Settings()));
+                },
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Paramètres'),
-            onTap: () {
-              Navigator.pushReplacement(
-                  context, MaterialPageRoute(builder: (context) => Settings()));
-            },
-          ),
-          Divider(),
-          ListTile(
-            title: FutureBuilder(
-                future: PackageInfo.fromPlatform(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<PackageInfo> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      PackageInfo info = snapshot.data;
-                      return Text("${info.version}+${info.buildNumber}");
-                    }
-                  }
-                  return SizedBox();
-                }),
-          ),
-        ],
-      ),
-    );
+        ),
+        FutureBuilder(
+            future: PackageInfo.fromPlatform(),
+            builder:
+                (BuildContext context, AsyncSnapshot<PackageInfo> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  PackageInfo info = snapshot.data;
+                  return Container(
+                      padding: EdgeInsets.all(5.0), child: Text(info.version));
+                }
+              }
+              return SizedBox();
+            })
+      ],
+    ));
   }
 }
