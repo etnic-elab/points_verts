78f6073a9687bd3b57d51ea58662cce38b41a39a
Thomas Borlée <thomas@borlee.be>
2020-03-28 14:54:47 +0100

    Added back weather in walk details screen

--- lib/views/walks/walk_details.dart
+++ lib/views/walks/walk_details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:points_verts/models/walk.dart';
+import 'package:points_verts/models/weather.dart';
+import 'package:points_verts/services/openweather.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../list_header.dart';
@@ -16,6 +18,8 @@ class WalkDetails extends StatelessWidget {
     return Expanded(
       child: ListView(
         children: <Widget>[
+          ListHeader("Prévisions météo"),
+          ListTile(title: _weather()),
           ListHeader("Autres informations"),
           ListTile(
             leading: Column(
@@ -60,10 +64,40 @@ class WalkDetails extends StatelessWidget {
     if (walk.trip != null) {
       return Text(
           "À ${walk.getFormattedDistance()}, ~${Duration(seconds: walk.trip.duration.round()).inMinutes} min. en voiture");
-    } else if (walk.distance != null) {
+    } else if (walk.distance != null && walk.distance != double.maxFinite) {
       return Text("À ${walk.getFormattedDistance()} (à vol d'oiseau)");
     } else {
       return null;
     }
+  }
+
+  Widget _weather() {
+    return FutureBuilder(
+        future: walk.weathers,
+        builder: (BuildContext context, AsyncSnapshot<List<Weather>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              List<Weather> weathers = snapshot.data;
+              List<Widget> widgets = List<Widget>();
+              for (Weather weather in weathers) {
+                widgets.add(Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("${weather.timestamp.hour}h", textScaleFactor: 0.8),
+                    getWeatherIcon(weather, context),
+                    Text("${weather.temperature.round()}°",
+                        textScaleFactor: 0.8),
+                    Text("${weather.windSpeed.round()} km/h",
+                        textScaleFactor: 0.8)
+                  ],
+                ));
+              }
+              return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: widgets);
+            }
+          }
+          return SizedBox();
+        });
   }
 }
