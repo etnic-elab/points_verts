dac707bda4cf71d703d1de8db1900f13686c0150
Thomas Borlée <thomas@borlee.be>
2020-03-28 14:36:41 +0100

    Improved walk details list

--- lib/views/walks/walk_details.dart
+++ lib/views/walks/walk_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:points_verts/models/walk.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../list_header.dart';
 import 'walk_utils.dart';
@@ -15,34 +16,52 @@ class WalkDetails extends StatelessWidget {
     return Expanded(
       child: ListView(
         children: <Widget>[
-          ListHeader("Lieu de rendez-vous"),
+          ListHeader("Autres informations"),
           ListTile(
-            dense: true,
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[Icon(Icons.location_on)],
+            ),
             title: Text(walk.meetingPoint),
-            subtitle: getGeoText(),
+            subtitle: _getGeoText(),
             onTap: () => launchGeoApp(walk),
           ),
-          ListHeader("Groupement organisateur"),
+          Divider(height: 0.5),
           ListTile(
-              dense: true,
-              title: Text("${walk.organizer}"),
-              subtitle: Text(
-                  "${walk.contactFirstName} ${walk.contactLastName} - ${walk.contactPhoneNumber != null ? walk.contactPhoneNumber : ''}")),
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[Icon(Icons.group)],
+            ),
+            title: Text("${walk.organizer}"),
+            subtitle: Text(
+                "${walk.contactFirstName} ${walk.contactLastName} - ${walk.contactPhoneNumber != null ? walk.contactPhoneNumber : ''}"),
+            onTap: () {
+              if (walk.contactPhoneNumber != null) {
+                launch("tel:${walk.contactPhoneNumber}");
+              }
+            },
+          ),
+          Divider(height: 0.5),
           walk.transport != null
-              ? ListHeader("Gare/Transport en commun")
+              ? ListTile(
+                  leading: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[Icon(Icons.train)],
+                  ),
+                  title: Text(walk.transport))
               : SizedBox.shrink(),
-          walk.transport != null
-              ? ListTile(dense: true, title: Text(walk.transport))
-              : SizedBox.shrink()
+          walk.transport != null ? Divider(height: 0.5) : SizedBox.shrink(),
         ],
       ),
     );
   }
 
-  Widget getGeoText() {
+  Widget _getGeoText() {
     if (walk.trip != null) {
       return Text(
-          "À ${walk.getFormattedDistance()}, environ ${Duration(seconds: walk.trip.duration.round()).inMinutes} minutes en voiture");
+          "À ${walk.getFormattedDistance()}, ~${Duration(seconds: walk.trip.duration.round()).inMinutes} min. en voiture");
+    } else if (walk.distance != null) {
+      return Text("À ${walk.getFormattedDistance()} (à vol d'oiseau)");
     } else {
       return null;
     }
