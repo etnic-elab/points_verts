fcafb63c4d26e6630d3c1440141d7fce8b8a9dac
Thomas Borlée <thomas@borlee.be>
2020-09-24 08:59:10 +0200

    Bump version and build number

--- lib/views/settings/about.dart
+++ lib/views/settings/about.dart
@@ -16,7 +16,7 @@ class About extends StatelessWidget {
               applicationIcon:
                   Image(image: AssetImage('assets/logo.png'), height: 50),
               applicationName: "Points Verts",
-              applicationVersion: "1.3.4",
+              applicationVersion: "1.3.5",
               applicationLegalese: "GNU GPLv3",
               children: [
                 _AboutRow("Dépôt du code source", "GitHub",
