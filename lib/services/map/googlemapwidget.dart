96575f83fe93bc1449744287bff4ba4b81406fa9
Sofyan Bokorna <sofyan.bokorna@etnic.be>
2021-11-04 12:10:54 +0100

    Improves the marker icons on the walks map
    Reindents ios/Runner/Info.plist

--- lib/services/map/googlemapwidget.dart
+++ lib/services/map/googlemapwidget.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
-import 'dart:ui';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:points_verts/models/walk.dart';
 import 'package:points_verts/views/walks/walk_tile.dart';
@@ -71,13 +73,26 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
     return assetPath;
   }
 
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
   // generate the walk icons from the assets
   void setWalkIcons() async {
-    walkIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(), getAssetPath('logo90'));
+    final Uint8List markerWalkIcon =
+        await getBytesFromAsset(getAssetPath("logo"), 70);
 
-    walkCanceledIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(), getAssetPath("logo-annule90"));
+    final Uint8List markerCanceledWalkIcon =
+        await getBytesFromAsset(getAssetPath("logo-annule"), 70);
+
+    walkIcon = BitmapDescriptor.fromBytes(markerWalkIcon);
+    walkCanceledIcon = BitmapDescriptor.fromBytes(markerCanceledWalkIcon);
   }
 
   @override
