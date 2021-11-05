89f70daeb8b892bbd7631563e2aa6c7fe54f7091
Sofyan Bokorna <sofyan.bokorna@etnic.be>
2021-11-05 16:09:07 +0100

    Adds google maps API keys with restrictions in iOS and Android configuration files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("SET_GOOGLE_API_KEY")
+    GMSServices.provideAPIKey("AIzaSyCtJ-RZfX6h3GJM8OiGGefodNkM0MV8ri8")
     GeneratedPluginRegistrant.register(with: self)
     UNUserNotificationCenter.current().delegate = self
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
