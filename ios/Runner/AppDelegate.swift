39e28e617f33cd20b5a9940349c3c5db58da5cc1
Thomas Borlée <thomas@massiverocket.com>
2021-11-19 20:29:18 +0100

    First pass at fixing iOS integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,13 +5,12 @@ import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"MAPS_API_KEY"];
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     if let value = ProcessInfo.processInfo.environment["GOOGLEMAPS_API_KEY_IOS"] {
-        GMSServices.provideAPIKey(value) 
-    } 
+        GMSServices.provideAPIKey(value)
+    }
     GeneratedPluginRegistrant.register(with: self)
     UNUserNotificationCenter.current().delegate = self
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
