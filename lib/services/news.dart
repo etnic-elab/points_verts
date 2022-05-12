cd31e39a8fd55930f55a68bc0e30abbe29503111
Cellule Innovation - ETNIC <82143806+innovation-elab@users.noreply.github.com>
2022-05-12 16:00:59 +0200

    NewsFix (#52)
    * ok
    
    * replace _firstLaunch
    
    * recheck for mounted and put back the newsRunning check. Put showNews method in service
    
    * small tweak
    
    * tweak
    
    Co-authored-by: Matthieu Simonart <matthieu.simonart@hi10.be>

--- lib/services/news.dart
+++ lib/services/news.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:points_verts/models/news.dart';
+import 'package:collection/collection.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:points_verts/models/news_seen.dart';
+import 'package:points_verts/services/prefs.dart';
+import 'package:points_verts/views/news.dart';
 
 const String url = "https://points-verts-328310.ew.r.appspot.com/live_news";
 
@@ -18,4 +23,58 @@ Future<List<News>> retrieveNews() async {
     print('Failed to load news');
   }
   return news;
+}
+
+Future<void> showNews(BuildContext context, bool mounted) async {
+  DateTime now = DateTime.now();
+
+  String? lastFetch = await PrefsProvider.prefs.getString(Prefs.lastNewsFetch);
+  bool doFetchNews = DateTime.tryParse(lastFetch ?? '')
+          ?.add(const Duration(days: 1))
+          .isBefore(now) ??
+      true;
+
+  if (doFetchNews) {
+    try {
+      List<dynamic> futures = await Future.wait(
+          [PrefsProvider.prefs.getString(Prefs.news), retrieveNews()]);
+
+      List oldNewsJson = jsonDecode(futures[0] ?? '[]');
+      List<NewsSeen> oldNews = [];
+      oldNews =
+          oldNewsJson.map<NewsSeen>((json) => NewsSeen.fromJson(json)).toList();
+
+      List<News> news = futures[1];
+      List<News> newsToShow = [];
+      for (News _news in news) {
+        NewsSeen? seen = oldNews
+            .firstWhereOrNull((NewsSeen seen) => _news.name == seen.name);
+        bool showNews = seen == null ||
+            (_news.intervalHours != null &&
+                seen.at
+                    .add(Duration(hours: _news.intervalHours!))
+                    .isBefore(now));
+
+        if (showNews) newsToShow.add(_news);
+      }
+
+      if (newsToShow.isNotEmpty && mounted) {
+        await showNewsDialog(context, newsToShow);
+
+        for (News shown in newsToShow) {
+          NewsSeen? seen = oldNews
+              .firstWhereOrNull((NewsSeen seen) => shown.name == seen.name);
+          seen == null
+              ? oldNews.add(NewsSeen.fromNews(shown, now))
+              : seen.at = now;
+        }
+        PrefsProvider.prefs.setString(Prefs.news, jsonEncode(oldNews));
+      }
+    } catch (err) {
+      print('Unable to show news: $err');
+    }
+
+    await PrefsProvider.prefs
+        .setString(Prefs.lastNewsFetch, now.toIso8601String());
+  }
 }
