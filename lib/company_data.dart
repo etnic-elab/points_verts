dc8bd085aa2b67d7f7c57d0b0c6a317cd7451665
innovation-elab <innovation@etnic.be>
2024-09-17 13:49:29 +0200

    merge master

--- lib/company_data.dart
+++ lib/company_data.dart
@@ -41,8 +41,9 @@ class CompanyColors {
 }
 
 class CompanyTheme {
-  static final greenSecondaryMatCol =
-      _createMaterialColor(CompanyColors.greenSecondary);
+  static final greenSecondaryMatCol = _createMaterialColor(
+    CompanyColors.greenSecondary,
+  );
 
   static final orangeMatCol = _createMaterialColor(
     CompanyColors.orange,
