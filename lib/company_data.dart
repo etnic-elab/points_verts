8dc7a0ed60323c124cac3355fe156ea60e1b11b3
innovation-elab <innovation@etnic.be>
2024-09-17 14:02:55 +0200

    remove materialColor creation by using colorSchemeSeed

--- lib/company_data.dart
+++ lib/company_data.dart
@@ -41,40 +41,12 @@ class CompanyColors {
 }
 
 class CompanyTheme {
-  static final greenSecondaryMatCol = _createMaterialColor(
-    CompanyColors.greenSecondary,
-  );
-
-  static final orangeMatCol = _createMaterialColor(
-    CompanyColors.orange,
-  );
-
   static final ThemeData companyLight = ThemeData(
-    colorSchemeSeed: orangeMatCol,
+    colorSchemeSeed: CompanyColors.orange,
   );
 
   static final ThemeData companyDark = ThemeData(
     brightness: Brightness.dark,
-    colorSchemeSeed: orangeMatCol,
+    colorSchemeSeed: CompanyColors.orange,
   );
-
-  static MaterialColor _createMaterialColor(Color color) {
-    List strengths = <double>[.05];
-    final swatch = <int, Color>{};
-    final int r = color.red, g = color.green, b = color.blue;
-
-    for (int i = 1; i < 10; i++) {
-      strengths.add(0.1 * i);
-    }
-    for (var strength in strengths) {
-      final double ds = 0.5 - strength;
-      swatch[(strength * 1000).round()] = Color.fromRGBO(
-        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-        1,
-      );
-    }
-    return MaterialColor(color.value, swatch);
-  }
 }
