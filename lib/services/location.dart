585d7f9cc7f21b674ede329ad79ac3b13d5194ef
innovation-elab <innovation@etnic.be>
2024-11-03 18:40:42 +0100

    Fix walk fetching for new installs and location permission request

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -2,12 +2,10 @@ import 'package:geolocator/geolocator.dart';
 
 Future<LocationPermission> checkLocationPermission() async {
   LocationPermission permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.deniedForever) {
-    await Geolocator.openLocationSettings();
-    return permission;
-  } else if (permission == LocationPermission.denied) {
+
+  if (permission == LocationPermission.denied) {
     return Geolocator.requestPermission();
-  } else {
-    return permission;
   }
+
+  return permission;
 }
